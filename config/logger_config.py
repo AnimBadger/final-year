ba11a037d2d8b19b6b0f1bd656be02bad789e79e
AnimBadger <anim.ansah.stephen@gmail.com>
2024-06-10 02:24:13 +0000

    configured logs to show on console

--- config/logger_config.py
+++ config/logger_config.py
@@ -1,5 +1,5 @@
 import logging
-
+import sys
 
 logging.basicConfig(
     filename='loggers.log',
@@ -7,4 +7,9 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
 
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+
 logger = logging.getLogger(__name__)
+logger.addHandler(console_handler)
