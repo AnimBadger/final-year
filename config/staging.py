d44ec759e9684952c0d76b19c2890137b099c858
animbadger <anim.ansah.stephen@gmail.com>
2024-07-15 23:25:54 +0000

    changed staging mongo properties

--- config/staging.py
+++ config/staging.py
@@ -1,4 +1,17 @@
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+MONGO_PASSWORD = os.getenv('MONGO_PASSWORD')
+MONGO_USERNAME = os.getenv('MONGO_USERNAME')
+MONGO_DB = os.getenv('MONGO_DB')
+
+
 class Setting:
-    MONGODB_URI = 'mongodb://mongo:27017/ed_proj'
+    MONGODB_URI = (f'mongodb+srv://{MONGO_USERNAME}:{MONGO_PASSWORD}@proj.pnwnb6u.mongodb.net/{MONGO_DB}?retryWrites'
+                   f'=true&w'
+                   f'=majority'
+                   '&appName=proj')
     DEBUG = True
     LOG_LEVEL = 'DEBUG'
