4ee8eb04f27b74a978e470f2568c9f31d6ab18df
animbadger <anim.ansah.stephen@gmail.com>
2024-08-04 14:40:19 +0000

    modified endpoint to accept base64 string and return file instead of the file id

--- router/base/base_txt_to_twi.py
+++ router/base/base_txt_to_twi.py
@@ -1,16 +1,19 @@
 import io
-from fastapi import APIRouter, HTTPException, status, UploadFile, File, Depends, Request
+from bson import Binary
+from fastapi.responses import StreamingResponse
+import filetype
+import base64
+from fastapi import APIRouter, HTTPException, Response, status, Depends, Request
 from motor.motor_asyncio import AsyncIOMotorClient
 from config import get_setting
-from fastapi.responses import StreamingResponse
 from model.upload_file_model import UploadResponseModel
+from model.upload_file_model import FileUploadModel
 from model.download_audio_model import CommentModel
 import uuid
-import os
 from config.logger_config import logger
 from config.jwt_config import get_current_user
 from model.jwt_model import TokenData
-from datetime import datetime
+from datetime import datetime, timezone
 from converter import (docx_to_text_converter,
                        doc_to_text_conveter,
                        pdf_to_text_converter,
@@ -33,7 +36,62 @@ def is_allowed_extension(filename: str) -> bool:
     return any(filename.endswith(ext) for ext in ALLOWED_EXTENSIONS)
 
 
-@router.post('/upload/{c_type}', response_model=UploadResponseModel, status_code=status.HTTP_201_CREATED)
+@router.post('/upload/{c_type}', status_code=status.HTTP_201_CREATED)
+async def upload_file(c_type: str, request: Request, upload_request: FileUploadModel, 
+                      token_data: TokenData = Depends(get_current_user)):
+    session_id = request.state.session_id
+    base64_string = upload_request.file
+
+    # Decode the base64 string to binary data
+    try:
+        file_data = base64.b64decode(base64_string)
+    except Exception:
+        logger.info(f'[{session_id}] error decoding base64 string')
+        raise HTTPException(status_code=400, detail='Invalid base64 string')
+
+    # Detect the file type
+    kind = upload_request.file_extention
+    if kind is None or kind not in ['txt', 'pdf', 'docx', 'doc']:
+        logger.info(f'[{session_id}] error uploading file')
+        raise HTTPException(status_code=400, detail='Invalid file type. Only .txt, .pdf, .docx, or .doc files are allowed.')
+
+    if c_type.lower() != 'summarize' and c_type.lower() != 'full':
+        raise HTTPException(status_code=400, detail='Bad request, path parameter requires type, summarize or full')
+
+    file_id = str(uuid.uuid4())
+    file_name = upload_request.file_name
+
+    # Add converters here
+    converters = {
+        'docx': docx_to_text_converter.convert_docx_to_text,
+        'doc': doc_to_text_conveter.convert_doc_to_text,
+        'pdf': pdf_to_text_converter.convert_pdf_to_text,
+        'txt': txt_to_text_conveter.convert_txt_to_text
+    }
+    conversion_function = converters.get(kind)
+    text = await conversion_function(file_data, file_name)
+
+    upload_dispatch = {
+        'username': token_data.username,
+        'file_name': file_name
+    }
+
+    if c_type.lower() == 'summarize':
+        audio_file = await summary_api.get_summary(text, upload_dispatch)
+    else:
+        audio_file = await text_to_twi_api.convert_to_twi(text, upload_dispatch)
+
+    file_record = {
+        '_id': file_id,
+        'file_name': file_name,
+        'content_type': kind,
+        'username': token_data.username,
+        'created_at': datetime.now(timezone.utc)
+    }
+    await uploads_collection.insert_one(file_record)
+    return Response(content=audio_file, media_type="audio/mpeg")
+
+'''
 async def upload_file(c_type: str, request: Request, file: UploadFile = File(...),
                       token_data: TokenData = Depends(get_current_user)):
     session_id = request.state.session_id
@@ -80,7 +138,7 @@ async def upload_file(c_type: str, request: Request, file: UploadFile = File(...
     }
     await uploads_collection.insert_one(file_data)
     return UploadResponseModel(message='Success', file_id=audio_id)
-
+'''
 
 @router.get('/{audio_id}/download')
 async def download_audio(audio_id: str, _: TokenData = Depends(get_current_user)):
