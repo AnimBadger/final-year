66fcb631d91a31ef04d1f7136859e311c58af29f
animbadger <anim.ansah.stephen@gmail.com>
2024-08-21 07:31:13 +0000

    handled txt file bug

--- converter/txt_to_text_conveter.py
+++ converter/txt_to_text_conveter.py
@@ -1,5 +1,5 @@
 from .regex_preprocessor import preprocess_text
 
-async def convert_txt_to_text(file_data: bytes) -> str:
+async def convert_txt_to_text(file_data: bytes, _) -> str:
     extracted_text = file_data.decode('utf-8')
     return preprocess_text(extracted_text)
